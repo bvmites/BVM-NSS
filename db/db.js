03e5036daf169a1b045ae773a4de5432eb7847a2
Manas Gabani <manasdineshgabani@gmail.com>
2019-07-11 23:18:39 +0530

    created login and signup methods

--- db/db.js
+++ db/db.js
@@ -1,24 +1,28 @@
 const {MongoClient,ObjectID} = require('mongodb');
 
 module.exports = (db) => ({
+
 	getReport : (year,event)=>{
 		return db.collection('Report').findOne({
 			year,
 			event
 		})
 	},
+
 	getPhotos : (year,event)=>{
 		return db.collection('Activities').findOne({
 			year,
 			event
 		})
 	},
+
 	login : (email,password)=>{
 		return db.collection('Users').findOne({
 			email,
 			password
 		})
 	},
+	
 	signup : (email,password,contact,name,res)=>{
 		db.collection('Users').insertOne({
 			name,
