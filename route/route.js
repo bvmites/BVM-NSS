ccac69fa1900597f8fac6c6e453e0131a329862e
Manas Gabani <manasdineshgabani@gmail.com>
2019-07-12 11:20:31 +0530

    params to query

--- route/route.js
+++ route/route.js
@@ -4,9 +4,9 @@ module.exports = (db) => {
 
 	const database = require('./../db/db.js')(db);
 
-	router.post('/getReport/:year/:event', async (req,res)=>{
-		// console.log(req.params.year,req.params.event)
-		var report = await database.getReport(req.params.year,req.params.event)
+	router.post('/getReport', async (req,res)=>{
+		// console.log(req.query)
+		var report = await database.getReport(req.query.year,req.query.event)
 		if (report) {
 			res.status(200).send({
 				status: 'Yes',
@@ -20,8 +20,8 @@ module.exports = (db) => {
 		}
 	})
 
-	router.post('/getPhotos/:year/:event', async (req,res)=>{
-		var images = await database.getPhotos(req.params.year,req.params.event)
+	router.post('/getPhotos', async (req,res)=>{
+		var images = await database.getPhotos(req.query.year,req.query.event)
 		if(images) {
 			res.status(200).send({
 				status:'Yes',
@@ -36,8 +36,8 @@ module.exports = (db) => {
 		}
 	})
 
-	router.post('/login/:email/:password',async(req,res)=>{
-		var user = await database.login(req.params.email,req.params.password)
+	router.post('/login',async(req,res)=>{
+		var user = await database.login(req.query.email,req.query.password)
 		if(user){
 			res.status(200).send({
 				status:'Yes'
@@ -49,9 +49,9 @@ module.exports = (db) => {
 		}
 	})
 
-	router.post('/signup/:name/:email/:password/:contact',(req,res)=>{
+	router.post('/signup',(req,res)=>{
 		// console.log(req.params)
-		database.signup(req.params.email,req.params.password,req.params.contact,req.params.name,res)
+		database.signup(req.query.email,req.query.password,req.query.contact,req.query.name,res)
 	})
 
 	return router;
